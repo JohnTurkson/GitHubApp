f6ad90267c7865f29cea63f390f1250220bb9599
John Turkson <github@johnturkson.com>
2020-01-13 17:47:09 -0800

    Refactor main activity layout (#1)
    * Update JUnit4 to JUnit5; Add dependencies
    
    * Exclude some META-INF subdirectories
    
    * Change AppTheme to Theme.AppCompat.Light.NoActionBar
    
    * Create settings and bottom navigation menus
    
    * Add menu icons
    
    * Add navigation to placeholder results page

--- app/src/main/java/com/johnturkson/githubapp/activities/MainActivity.kt
+++ app/src/main/java/com/johnturkson/githubapp/activities/MainActivity.kt
@@ -1,15 +1,22 @@
 package com.johnturkson.githubapp.activities
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.Menu
+import android.view.MenuItem
+import android.view.View
 import android.widget.Button
 import android.widget.EditText
 import android.widget.TextView
+import android.widget.Toast
+import androidx.appcompat.widget.Toolbar
 import androidx.constraintlayout.widget.ConstraintLayout
 import androidx.databinding.DataBindingUtil
 import androidx.databinding.library.BuildConfig
 import androidx.recyclerview.widget.LinearLayoutManager
 import androidx.recyclerview.widget.RecyclerView
+import com.google.android.material.bottomnavigation.BottomNavigationView
 import com.johnturkson.githubapp.R
 import com.johnturkson.githubapp.adapters.GitHubRepositoryAdapter
 import com.johnturkson.githubapp.api.GitHubApi
@@ -26,10 +33,12 @@ class MainActivity : AppCompatActivity() {
     
     private lateinit var binding: ActivityMainBinding
     private lateinit var mainActivityLayout: ConstraintLayout
+    private lateinit var toolbar: Toolbar
     private lateinit var searchBar: EditText
     private lateinit var searchButton: Button
     private lateinit var repositoryViewTitle: TextView
     private lateinit var repositoryView: TextView
+    private lateinit var bottomNavigationView: BottomNavigationView
     
     private lateinit var repositories: RecyclerView
     private lateinit var repositoriesViewAdapter: GitHubRepositoryAdapter
@@ -46,6 +55,9 @@ class MainActivity : AppCompatActivity() {
         binding = DataBindingUtil.setContentView(this, R.layout.activity_main)
         
         mainActivityLayout = binding.mainActivityLayout
+        toolbar = binding.mainToolbar
+        setSupportActionBar(toolbar)
+        toolbar.setNavigationOnClickListener { onToolbarNavigationClicked(it) }
         searchBar = binding.userSearchBar
         searchButton = binding.searchButton
         repositoryViewTitle = binding.repositoryResultsTitle
@@ -54,10 +66,59 @@ class MainActivity : AppCompatActivity() {
             layoutManager = repositoriesViewManager
             adapter = repositoriesViewAdapter
         }
+        bottomNavigationView = binding.navViewBottom
+        bottomNavigationView.setOnNavigationItemSelectedListener { onBottomNavigationViewItemSelected(it) }
         
         binding.githubRepositories.adapter = GitHubRepositoryAdapter()
         searchButton.setOnClickListener {
-            searchForUser(searchBar.text.toString())
+            searchForRepositories(searchBar.text.toString())
+        }
+    }
+    
+    override fun onCreateOptionsMenu(menu: Menu?): Boolean {
+        Timber.d("onCreateOptionsMenu invoked")
+        menuInflater.inflate(R.menu.settings_menu, menu)
+        Timber.d("options menu inflated")
+        return super.onCreateOptionsMenu(menu)
+    }
+    
+    override fun onOptionsItemSelected(item: MenuItem): Boolean {
+        // TODO implement toolbar menu
+        Timber.d("toolbar options item selected")
+        return when (item.itemId) {
+            R.id.settings -> Toast.makeText(this, "settings", Toast.LENGTH_SHORT).show()
+                .also { Timber.d("settings menu item selected") }
+                .let { true }
+            else -> super.onOptionsItemSelected(item)
+        }
+    }
+    
+    private fun onToolbarNavigationClicked(view: View) {
+        Timber.d("toolbar navigation menu button clicked")
+        // TODO open navigation drawer
+        Toast.makeText(this, "navigation selected", Toast.LENGTH_SHORT).show()
+    }
+    
+    // TODO refactor into object to reduce duplication (pass in context)
+    private fun onBottomNavigationViewItemSelected(item: MenuItem): Boolean {
+        // TODO implement bottom navigation
+        Timber.d("bottom navigation view item selected")
+        // TODO remove
+        val testIntent = Intent(this, RepositorySearchResultsActivity::class.java)
+        return when (item.itemId) {
+            R.id.navigation_search -> Toast.makeText(this, "search", Toast.LENGTH_SHORT).show()
+                .also { Timber.d("search tab selected") }
+                .let { true }
+            R.id.navigation_users -> startActivity(testIntent)
+                .also { Timber.d("users tab selected") }
+                .let { true }
+            // R.id.navigation_users -> Toast.makeText(this, "users", Toast.LENGTH_SHORT).show()
+            //     .also { Timber.d("user tab selected") }
+            //     .let { true }
+            R.id.navigation_repositories -> Toast.makeText(this, "repositories", Toast.LENGTH_SHORT).show()
+                .also { Timber.d("repositories tab selected") }
+                .let { true }
+            else -> super.onOptionsItemSelected(item)
         }
     }
     
@@ -71,7 +132,7 @@ class MainActivity : AppCompatActivity() {
         Timber.i("started timber")
     }
     
-    private fun searchForUser(username: String) {
+    private fun searchForRepositories(username: String) {
         Timber.d("request made for User $username")
         GitHubApi.client
             .getRepositories(username)
