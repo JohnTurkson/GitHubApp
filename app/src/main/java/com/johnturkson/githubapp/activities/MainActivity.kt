1fcae50f06fb9395196efe27093c237b1749b79e
John Turkson <github@johnturkson.com>
2020-01-14 17:17:24 -0800

    Add new activity to display user search results

--- app/src/main/java/com/johnturkson/githubapp/activities/MainActivity.kt
+++ app/src/main/java/com/johnturkson/githubapp/activities/MainActivity.kt
@@ -1,7 +1,6 @@
 package com.johnturkson.githubapp.activities
 
 import android.content.Intent
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.view.Menu
 import android.view.MenuItem
@@ -10,6 +9,7 @@ import android.widget.Button
 import android.widget.EditText
 import android.widget.TextView
 import android.widget.Toast
+import androidx.appcompat.app.AppCompatActivity
 import androidx.appcompat.widget.Toolbar
 import androidx.constraintlayout.widget.ConstraintLayout
 import androidx.databinding.DataBindingUtil
@@ -24,14 +24,18 @@ import com.johnturkson.githubapp.api.GitHubApi
 import com.johnturkson.githubapp.api.GitHubRepository
 import com.johnturkson.githubapp.api.GitHubUser
 import com.johnturkson.githubapp.databinding.ActivityMainBinding
+import kotlinx.coroutines.*
 import kotlinx.serialization.UnstableDefault
+import kotlinx.serialization.list
 import retrofit2.Call
 import retrofit2.Callback
 import retrofit2.Response
+import retrofit2.await
 import timber.log.Timber
+import kotlin.coroutines.CoroutineContext
 
 @UnstableDefault
-class MainActivity : AppCompatActivity() {
+class MainActivity : AppCompatActivity(), CoroutineScope {
     
     private lateinit var binding: ActivityMainBinding
     private lateinit var mainActivityLayout: ConstraintLayout
@@ -39,38 +43,12 @@ class MainActivity : AppCompatActivity() {
     private lateinit var searchBar: EditText
     private lateinit var searchButton: Button
     private lateinit var repositoryViewTitle: TextView
-    private lateinit var repositoryView: TextView
-    private lateinit var repositories: RecyclerView
+    private lateinit var repositoriesView: RecyclerView
     private lateinit var repositoriesViewManager: RecyclerView.LayoutManager
-    private lateinit var repositoriesViewAdapter: RecyclerView.Adapter<*>
+    private lateinit var repositoriesViewAdapter: RecyclerView.Adapter<GitHubRepositoryViewHolder>
     private lateinit var bottomNavigationView: BottomNavigationView
     
-    val repos =listOf(
-        GitHubRepository(
-            1,
-            "john",
-            GitHubUser("john", 1, "nodeid", "avatar", "url"),
-            "example repo",
-            "this is an example repo",
-            "example description"
-        ),
-        GitHubRepository(
-            2,
-            "john",
-            GitHubUser("john2", 1, "nodeid", "avatar", "url"),
-            "example repo2",
-            "this is an example repo",
-            "example description"
-        ),
-        GitHubRepository(
-            3,
-            "john",
-            GitHubUser("john3", 1, "nodeid", "avatar", "url"),
-            "example repo3",
-            "this is an example repo",
-            "example description"
-        )
-    )
+    private val repositories = mutableListOf<GitHubRepository>()
     
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -80,32 +58,29 @@ class MainActivity : AppCompatActivity() {
         binding = DataBindingUtil.setContentView(this, R.layout.activity_main)
         
         mainActivityLayout = binding.mainActivityLayout
+        
         toolbar = binding.mainToolbar
         setSupportActionBar(toolbar)
         toolbar.setNavigationOnClickListener { onToolbarNavigationClicked(it) }
+        
         searchBar = binding.userSearchBar
         searchButton = binding.searchButton
-        repositoryViewTitle = binding.repositoryResultsTitle
-        repositoryView = binding.repositoryResults
+        searchButton.setOnClickListener {
+            searchForUsers(searchBar.text.toString())
+            // searchForRepositories(searchBar.text.toString())
+        }
         
+        repositoryViewTitle = binding.repositoryResultsTitle
         repositoriesViewManager = LinearLayoutManager(this)
-        repositoriesViewAdapter = GitHubRepositoryViewAdapter(
-            repos
-        )
-        repositories = binding.githubRepositories.apply {
+        repositoriesViewAdapter = GitHubRepositoryViewAdapter(repositories)
+        repositoriesView = binding.githubRepositories.apply {
             layoutManager = repositoriesViewManager
             adapter = repositoriesViewAdapter
         }
         
         bottomNavigationView = binding.navViewBottom
         bottomNavigationView.setOnNavigationItemSelectedListener {
-            onBottomNavigationViewItemSelected(
-                it
-            )
-        }
-        
-        searchButton.setOnClickListener {
-            searchForRepositories(searchBar.text.toString())
+            onBottomNavigationViewItemSelected(it)
         }
     }
     
@@ -137,18 +112,21 @@ class MainActivity : AppCompatActivity() {
     private fun onBottomNavigationViewItemSelected(item: MenuItem): Boolean {
         // TODO implement bottom navigation
         Timber.d("bottom navigation view item selected")
-        // TODO remove
-        val testIntent = Intent(this, RepositorySearchResultsActivity::class.java)
         return when (item.itemId) {
-            R.id.navigation_search -> Toast.makeText(this, "search", Toast.LENGTH_SHORT).show()
+            R.id.navigation_search -> Toast.makeText(
+                this,
+                "search",
+                Toast.LENGTH_SHORT
+            ).show()
                 .also { Timber.d("search tab selected") }
                 .let { true }
-            R.id.navigation_users -> startActivity(testIntent)
-                .also { Timber.d("users tab selected") }
+            R.id.navigation_users -> Toast.makeText(
+                this,
+                "users",
+                Toast.LENGTH_SHORT
+            ).show()
+                .also { Timber.d("user tab selected") }
                 .let { true }
-            // R.id.navigation_users -> Toast.makeText(this, "users", Toast.LENGTH_SHORT).show()
-            //     .also { Timber.d("user tab selected") }
-            //     .let { true }
             R.id.navigation_repositories -> Toast.makeText(
                 this,
                 "repositories",
@@ -168,6 +146,28 @@ class MainActivity : AppCompatActivity() {
             Timber.plant(Timber.DebugTree())
         }
         Timber.i("started timber")
+    }
+    
+    // TODO refactor into separate object/class
+    private fun searchForUsers(name: String) {
+        launch {
+            Timber.d("request to find user with name $name")
+            val response = withContext(Dispatchers.IO) {
+                GitHubApi.client.searchUsers(name).await()
+            }
+            populateUserSearchResults(response.users)
+        }
+    }
+    
+    // TODO refactor into separate object/class
+    private fun populateUserSearchResults(users: List<GitHubUser>) {
+        val displayusersIntent = Intent(this, UserSearchResultsActivity::class.java)
+        val json = GitHubApi.encoder.stringify(GitHubUser.serializer().list, users)
+        Timber.v(json)
+        Timber.d("add users json as extra")
+        displayusersIntent.putExtra(UserSearchResultsActivity.EXTRA_USERS, json)
+        Timber.d("start activity to display users")
+        startActivity(displayusersIntent)
     }
     
     private fun searchForRepositories(username: String) {
@@ -207,12 +207,14 @@ class MainActivity : AppCompatActivity() {
     }
     
     private fun updateRepositoriesView(repositories: List<GitHubRepository>) {
-        require(repositories.isNotEmpty())
-        repositoryViewTitle.text = getString(R.string.repository_results_title_text)
-        repositoryView.text = repositories.random().name
-        // TODO not working
-        // repositoriesViewAdapter.repositories = repositories
-        // repositoriesViewAdapter.notifyDataSetChanged()
-        Timber.d("updated repository text view")
+        val displayRepositoriesIntent = Intent(this, RepositorySearchResultsActivity::class.java)
+        val json = GitHubApi.encoder.stringify(GitHubRepository.serializer().list, repositories)
+        Timber.v(json)
+        Timber.d("add json as extra")
+        displayRepositoriesIntent.putExtra(RepositorySearchResultsActivity.EXTRA_REPOSITORIES, json)
+        Timber.d("start activity to display results")
+        startActivity(displayRepositoriesIntent)
     }
+    
+    override val coroutineContext: CoroutineContext = Dispatchers.Main + SupervisorJob()
 }
